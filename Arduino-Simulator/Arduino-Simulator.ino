4571d4994a194b865a5881363a40a413706db52f
rathsf <rathsf@eikontx.com>
2024-01-12 11:02:40 -0800

    update

--- Arduino-Simulator/Arduino-Simulator.ino
+++ Arduino-Simulator/Arduino-Simulator.ino
@@ -14,7 +14,7 @@ boolean ser_col = false;
 boolean prototyping = true;
 
 uint8_t hurry = 16;
-const char *paletteNames[] = {"monochrome", "duotone", "pastel", "whacky"};
+const char *paletteNames[] = {"monochrome", "duotone", "pastel", "static"};
 
 #include "auxFnss/auxFnss.h"
 #include "palettes/palettes.h"
@@ -30,18 +30,22 @@ void setup()
 
   FastLED.addLeds < WS2812B, LED_PIN, GRB > (leds, NUM_LEDS);
   
-  Serial.println(); Serial.println("##### Hello Lamp #####");
-  
+  Serial.println();
+  Serial.println("######## Hello Lamp ########");
+  Serial.println("############################");
+
   generateNewHues(pllt, 30, false);
 
-  pllt.hueA = 20;
+  pllt.hueA = 30;
+  pllt.hueB = 130;
+  pllt.paletteType = "duotone";
 
   initializePerlin(500, 10000);
 
   scls.lumScales = {1000, 15000, 1000, 15000};
   scls.colScales = {1000, 15000, 1000, 15000};
 
-  paletteSelection(pllt, "monochrome");
+  paletteSelection(pllt, pllt.paletteType);
   changeScales(scls, 6000, true, false);
   triggerBlend(50, false);
   blendColors(pllt, true, true, false);
@@ -51,16 +55,23 @@ void setup()
 // ################## LOOP ####################
 void loop()
 {
-  EVERY_N_MILLISECONDS(6000)
+  int changes = 5000;
+
+  EVERY_N_MILLISECONDS(changes)
   {
     generateNewHues(pllt, 30, true);
+    // executed = false;
+
     Serial.println(pllt.hueA);
-    incrementPalette(pllt);
-    triggerBlend(500, true);
-    changeScales(scls, 4000, false, false);
+    Serial.println(pllt.hueB);
+
+    // incrementPalette(pllt);
+    paletteSelection(pllt, pllt.paletteType, true);
+    triggerBlend(changes*0.99, true);
+    // changeScales(scls, changes, false, false);
   }
 
-  paletteSelection(pllt, pllt.paletteType, true);
+  
   blendColors(pllt, true, true, false);
   makeNoise(pllt, hurry, true);
   FastLED.show();
--- Arduino-Simulator/palettes/palettes.h
+++ Arduino-Simulator/palettes/palettes.h
@@ -5,17 +5,17 @@ void paletteSelection(palette &palette, const String &paletteType, bool reportin
     {
         if (paletteType == "monochrome")
         {
-            palette.col[0] = {palette.hueA, 10, 155, 255, 155, 255};
+            palette.col[0] = {palette.hueA, 10, 55, 255, 155, 255};
             palette.col[1] = {palette.hueA, 10, 155, 255, 155, 255};
-            palette.col[2] = {palette.hueA, 10, 155, 255, 155, 255};
-            palette.col[3] = {palette.hueA, 10, 155, 255, 155, 255};
+            palette.col[2] = {palette.hueB, 10, 155, 255, 155, 255};
+            palette.col[3] = {palette.hueB, 10, 55, 255, 155, 255};
         }
         else if (paletteType == "duotone")
         {
-            palette.col[0] = {palette.hueA, 10, 155, 255, 155, 255};
+            palette.col[0] = {palette.hueA, 10, 55, 255, 155, 255};
             palette.col[1] = {palette.hueA, 10, 155, 255, 155, 255};
             palette.col[2] = {palette.hueB, 10, 155, 255, 155, 255};
-            palette.col[3] = {palette.hueB, 10, 155, 255, 155, 255};
+            palette.col[3] = {palette.hueB, 10, 55, 255, 155, 255};
         }
         else if (paletteType == "pastel")
         {
@@ -23,6 +23,13 @@ void paletteSelection(palette &palette, const String &paletteType, bool reportin
             palette.col[1] = {palette.hueA, 10, 5, 5, 155, 255};
             palette.col[2] = {palette.hueA, 10, 5, 5, 155, 255};
             palette.col[3] = {palette.hueA, 10, 5, 5, 155, 255};
+        }
+        else if (paletteType == "static")
+        {
+            palette.col[0] = {palette.hueA, 0, 255, 255, 255, 255};
+            palette.col[1] = {palette.hueA, 0, 255, 255, 255, 255};
+            palette.col[2] = {palette.hueA, 0, 255, 255, 255, 255};
+            palette.col[3] = {palette.hueA, 0, 255, 255, 255, 255};
         }
         else // "Unknown palette type"
         {   
